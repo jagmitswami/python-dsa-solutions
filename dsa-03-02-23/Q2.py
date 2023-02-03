674882672f8a9fe727aff9bac5a87e5b71cc037a
Jagmit <bitscotch@gmail.com>
2023-02-03 16:10:15 +0530

    added python dsa solutions

--- dsa-03-02-23/Q2.py
+++ dsa-03-02-23/Q2.py
@@ -4,7 +4,7 @@ Search for a name of a city input by the user in the list. If found, display* �
 print the name of the city along with its STD code, or else
 display the message “Search Unsuccessful, No such city in the list”.'''
 
-cities = ['delhi', 'pune', 'mumbai', 'chennai', 'hydrabad', 'kolkata', 'amritsar', 'indor', 'chandigarh', 'shimla']
+cities = ['delhi', 'pune', 'mumbai', 'chennai', 'hydrabad', 'kolkata', 'amritsar', 'indore', 'chandigarh', 'shimla']
 std = [1000, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999]
 
 print("Enter city name to check std: ")
